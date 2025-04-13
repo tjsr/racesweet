1c04aff08521b1548c6e70a2d10e6f025cd2186c
Tim Rowe <tim@tjsr.id.au>
2025-04-13 11:51:16 +1000

    Add vitest and ts/node core plus libs.

--- vitest.chdir.mts
+++ vitest.chdir.mts
@@ -2,5 +2,4 @@ import process from "process";
 import { vi } from "vitest";
 
 const pathValue =new URL(".", import.meta.url).pathname;
-console.log(pathValue);
 vi.spyOn(process, "cwd").mockReturnValue(pathValue);
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,10 @@
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import { dirname, resolve } from 'path';
+
+import { fileURLToPath } from 'url';
+
 export default [
-  'vitest.config.mts',
+  resolve(__dirname, 'vitest.config.mts'),
 ];
