157615ea46f0c09b2f25d957ddf4f6261b67d2bc
Tim Rowe <tim@tjsr.id.au>
2025-04-23 15:23:42 +1000

    Fix and handling of vitest configs.

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,10 +1,15 @@
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 import { dirname, resolve } from 'path';
 
 import { fileURLToPath } from 'url';
 
+const vitestConfigFile = 'vitest.config.mts';
+const vitestWorkspaceFile = 'vitest.workspace.ts'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fullConfigPath = resolve(__dirname, vitestConfigFile);
+console.debug(`vitest config file (${vitestConfigFile}) in ${vitestWorkspaceFile} resolves to ${fullConfigPath}.`);
+
 export default [
-  resolve(__dirname, 'vitest.config.mts'),
+  fullConfigPath,
 ];
